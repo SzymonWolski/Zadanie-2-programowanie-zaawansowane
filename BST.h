1758aff377b3b99556301cec4bab047c9a6f3422
ShiroKitsu <93395807+Bercik1287@users.noreply.github.com>
2024-11-14 19:23:39 +0100

    Czym jest czas
    Ludzie zakładają, że czas jest sztywno określonym postępem przyczyn do skutku, ale w rzeczywistości, z nieliniowego, niesubiektywnego punktu widzenia jest bardziej wielką kulką jakiegoś czasowego czegoś

--- BST.h
+++ BST.h
@@ -5,8 +5,10 @@
 #include <vector>
 #include <fstream>
 
+// Klasa BST - implementacja drzewa binarnego poszukiwañ
 class BST {
 private:
+    // Struktura wêz³a drzewa
     struct Node {
         int data;
         Node* left;
@@ -14,28 +16,29 @@ private:
         Node(int value) : data(value), left(nullptr), right(nullptr) {}
     };
 
-    Node* root;
+    Node* root; // Korzeñ drzewa
 
-    void addNode(Node*& node, int value);
-    Node* deleteNode(Node* node, int value);
-    void deleteTree(Node* node);
-    Node* findNode(Node* node, int value) const;
-    void preorder(Node* node) const;
-    void inorder(Node* node) const;
-    void postorder(Node* node) const;
-    void writeToTextFile(Node* node, std::ofstream& file) const;
+    // Metody prywatne pomocnicze
+    void addNode(Node*& node, int value);         // Dodaje wêze³ do drzewa
+    Node* deleteNode(Node* node, int value);      // Usuwa wêze³ z drzewa
+    void deleteTree(Node* node);                  // Usuwa ca³e drzewo
+    Node* findNode(Node* node, int value) const;  // Znajduje wêze³ o podanej wartoœci
+    void preorder(Node* node) const;              // Wyœwietla wêz³y w porz¹dku preorder
+    void inorder(Node* node) const;               // Wyœwietla wêz³y w porz¹dku inorder
+    void postorder(Node* node) const;             // Wyœwietla wêz³y w porz¹dku postorder
+    void writeToTextFile(Node* node, std::ofstream& file) const; // Zapisuje drzewo do pliku tekstowego
 
 public:
-    BST();
-    ~BST();
-    void add(int value);
-    void remove(int value);
-    void clear();
-    bool find(int value) const;
-    void printPreorder() const;
-    void printInorder() const;
-    void printPostorder() const;
-    void saveToTextFile(const std::string& filename) const;
+    BST();                  // Konstruktor
+    ~BST();                 // Destruktor
+    void add(int value);    // Dodaje wartoœæ do drzewa
+    void remove(int value); // Usuwa wartoœæ z drzewa
+    void clear();           // Usuwa ca³e drzewo
+    bool find(int value) const; // Sprawdza, czy wartoœæ znajduje siê w drzewie
+    void printPreorder() const; // Wyœwietla drzewo w porz¹dku preorder
+    void printInorder() const;  // Wyœwietla drzewo w porz¹dku inorder
+    void printPostorder() const; // Wyœwietla drzewo w porz¹dku postorder
+    void saveToTextFile(const std::string& filename) const; // Zapisuje drzewo do pliku tekstowego
 };
 
 #endif // BST_H
