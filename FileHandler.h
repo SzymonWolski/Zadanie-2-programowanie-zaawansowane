2724cc4b32d85867cc76942f4dff68976a03aee6
SzymonWolski <wolszym@o2.pl>
2024-11-14 19:01:38 +0100

    Pierwsza Wersja
    Działa jako tako

---
+++ FileHandler.h
@@ -0,0 +1,14 @@
+#ifndef FILEHANDLER_H
+#define FILEHANDLER_H
+
+#include "BST.h"
+#include <string>
+
+class FileHandler {
+public:
+    static void saveBinaryTree(const BST& tree, const std::string& filename);
+    static void loadBinaryTree(BST& tree, const std::string& filename);
+    static void loadFromTextFile(BST& tree, const std::string& filename);
+};
+
+#endif // FILEHANDLER_H
